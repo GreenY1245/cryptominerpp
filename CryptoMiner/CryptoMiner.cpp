702416a0854309af9084b259716e012ba80962fa
Klemen Plaznik <klemenik@gmail.com>
2020-01-07 22:21:23 +0100

    Add json library, ISO timestamp fetch, comments

--- CryptoMiner/CryptoMiner.cpp
+++ CryptoMiner/CryptoMiner.cpp
@@ -4,7 +4,8 @@
 #include <iostream>
 #include <string>
 #include <picosha2.h>
-#include <vector>
+#include <ctime>
+#include <json.hpp>
 
 std::string generateRandomString(const int length) {
 
@@ -25,13 +26,33 @@ int main() {
 	
 	while (true) {
 
+		// region Get timestamp
+		time_t now = time(nullptr);
+		struct tm timeinfo;
+		gmtime_s(&timeinfo, &now);
+		char timestamp[sizeof "YYYY-MM-DDTHH:MM:SSZ"];
+		strftime(timestamp, sizeof timestamp, "%FT%TZ", &timeinfo);
+		// endregion
+		
 		std::vector<unsigned char> hash(picosha2::k_digest_size);
-		random = generateRandomString(16) + std::to_string(nonce);
+		random = generateRandomString(16) + std::to_string(nonce) + timestamp;
 
+		// region Hash block
 		picosha2::hash256(random.begin(), random.end(), hash.begin(), hash.end());
+		// endregion
 
+
+		// Check if block hash matches required parameters
 		if (picosha2::bytes_to_hex_string(hash.begin(), hash.end()).substr(0, 2) == "00") {
-			std::cout << picosha2::bytes_to_hex_string(hash.begin(), hash.end()) << std::endl;
+
+			// JSON construction
+			nlohmann::json engine;
+			engine["timestamp"] = timestamp;
+			engine["data"] = random;
+			engine["hash"] = picosha2::bytes_to_hex_string(hash.begin(), hash.end());
+
+			// JSON output
+			std::cout << engine.dump() << std::endl;
 
 			// Temporary because there are no HTTP api calls at the moment but the hashing works
 			if (count > 5) {
